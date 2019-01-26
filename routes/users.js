ee4e0515b4855c7750ca72aa1f0f08e9297d0e5d
younes <youneskasri@gmail.com>
2019-01-26 22:11:48 +0100

    Signup Doctor OK

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,19 @@ const SELECT_COUNT_MEDECIN = `
   GROUP BY id_medecin
 `;
 
+const INSERT_MEDECIN = `
+  INSERT INTO medecin(nom, prenom, mail, password,
+    telephone, specialite, date_naissance, description,
+    adresse)
+  VALUES (?,?,?,? ,?,?,?,? ,?)
+`;
+
 /* GET users listing. */
 router.get('/login', (req, res) => res.render("login", {...req.query}))
   .post('/login', checkLogin, doLogin, (req, res) => res.redirect("/"))
-  .get('/logout', doLogout)
+  .use('/logout', doLogout, (req, res) => res.redirect("/login?logout=1"))
+  .get('/signup', doLogout, (req, res) => res.render("signup"))
+  .post('/signup', doSignup, doLogin, (req, res) => res.redirect("/"));
 
 function checkLogin(req, res, next) {
   const { email, password } = req.body;
@@ -34,7 +43,22 @@ function doLogin(req, res, next) {
 
 function doLogout(req, res, next) {
   req.session.logout()
-    .then(() => res.redirect("/login?logout=1"));
+    .then(() => next());
+}
+
+function doSignup(req, res, next) {
+    const { nom, prenom, email,  password, telephone, 
+      specialite, dateNaissance, description, adresse } = req.body;
+    let params = [ nom, prenom, email,  password, telephone, 
+      specialite, dateNaissance, description, adresse ];
+    req.getConnection(function(err, connection) {
+      if (err) return next(err);
+      connection.query(INSERT_MEDECIN, params, function(err, result) {
+        if (err) return next(err);
+        req.idMedecin = result.insertId;
+        next(); 
+      });
+    });
 }
 
 module.exports = router;
